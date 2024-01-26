ab97a0b5ed12c48551bd2301ea329a3605bf4f4b
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2024-01-27 05:20:11 +0700

    modify initialize db: Create database if not exist

--- internal/infrastructure/database/database.go
+++ internal/infrastructure/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -25,10 +26,24 @@ func InitializeDB() *gorm.DB {
 		panic("missing required environment variables")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s sslmode=disable", host, user, password, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to connect to database!")
+		panic("Failed to connect to PostgreSQL server!")
+	}
+
+	var count int
+	db.Raw("SELECT COUNT(*) FROM pg_database WHERE datname = ?", dbName).Scan(&count)
+	if count == 0 {
+		if err := db.Exec(fmt.Sprintf("CREATE DATABASE %s;", dbName)).Error; err != nil {
+			panic(fmt.Sprintf("Failed to create database: %s", err))
+		}
+		log.Printf("Database %s created successfully", dbName)
+	}
+
+	db, err = gorm.Open(postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, port)), &gorm.Config{})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database %s: %s", dbName, err))
 	}
 
 	return db
