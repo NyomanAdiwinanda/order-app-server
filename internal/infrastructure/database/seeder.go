935d22b0fc0cb376a03ae1ee3458024a1ef58edb
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2024-01-27 04:13:28 +0700

    fix CSV seeding

--- internal/infrastructure/database/seeder.go
+++ internal/infrastructure/database/seeder.go
@@ -6,15 +6,33 @@ import (
 )
 
 func SeedDB(db *gorm.DB) {
-	// Read CSV files
+	// Read CSV file
 	customerCompanyData, err := utils.ReadCSVFile("csv/customer_companies.csv")
 	if err != nil {
 		panic("Failed to read customer_companies.csv")
 	}
 
+	// Convert CSV data to struct slices
+	customerCompanies := utils.CSVToCustomerCompanies(customerCompanyData)
+
+	// Seed data into the database
+	for _, customerCompany := range customerCompanies {
+		db.Create(&customerCompany)
+	}
+
+	// Repeat same process for other CSV files
 	customerData, err := utils.ReadCSVFile("csv/customers.csv")
 	if err != nil {
 		panic("Failed to read customer.csv")
+	}
+
+	customers, err := utils.CSVToCustomers(customerData)
+	if err != nil {
+		panic("Failed to convert customers CSV")
+	}
+
+	for _, customer := range customers {
+		db.Create(&customer)
 	}
 
 	orderData, err := utils.ReadCSVFile("csv/orders.csv")
@@ -22,9 +40,27 @@ func SeedDB(db *gorm.DB) {
 		panic("Failed to read orders.csv")
 	}
 
+	orders, err := utils.CSVToOrders(db, orderData)
+	if err != nil {
+		panic("Failed to convert orders CSV")
+	}
+
+	for _, order := range orders {
+		db.Create(&order)
+	}
+
 	orderItemData, err := utils.ReadCSVFile("csv/order_items.csv")
 	if err != nil {
 		panic("Failed to read order_items.csv")
+	}
+
+	orderItems, err := utils.CSVToOrderItems(orderItemData)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, orderItem := range orderItems {
+		db.Create(&orderItem)
 	}
 
 	deliveryData, err := utils.ReadCSVFile("csv/deliveries.csv")
@@ -32,39 +68,11 @@ func SeedDB(db *gorm.DB) {
 		panic("Failed to read deliveries.csv")
 	}
 
-	// Convert CSV data to struct slices
-	customerCompanies := utils.CSVToCustomerCompanies(customerCompanyData)
-
-	customers, err := utils.CSVToCustomers(customerData)
-	if err != nil {
-		panic("Failed to convert customers CSV")
-	}
-
-	orders := utils.CSVToOrders(orderData)
-
-	orderItems, err := utils.CSVToOrderItems(orderItemData)
-	if err != nil {
-		panic(err)
-	}
-
 	deliveryItems, err := utils.CSVToDeliveries(deliveryData)
 	if err != nil {
 		panic(err)
 	}
 
-	// Seed data into the database
-	for _, customerCompany := range customerCompanies {
-		db.Create(&customerCompany)
-	}
-	for _, customer := range customers {
-		db.Create(&customer)
-	}
-	for _, order := range orders {
-		db.Create(&order)
-	}
-	for _, orderItem := range orderItems {
-		db.Create(&orderItem)
-	}
 	for _, delivery := range deliveryItems {
 		db.Create(&delivery)
 	}
--- internal/infrastructure/utils/CSVToOrders.go
+++ internal/infrastructure/utils/CSVToOrders.go
@@ -1,36 +1,36 @@
 package utils
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/NyomanAdiwinanda/order-app-server/internal/core/models"
+	"gorm.io/gorm"
 )
 
-func CSVToOrders(orderData [][]string) []models.Order {
+func CSVToOrders(db *gorm.DB, orderData [][]string) ([]models.Order, error) {
 	var Orders []models.Order
 	const layout = "2006-01-02T15:04:05Z"
 	location, _ := time.LoadLocation("Australia/Melbourne")
 
 	for _, row := range orderData[1:] {
-		var customerIdData string
-		if row[3] == "ivan" {
-			customerIdData = "1"
-		} else {
-			customerIdData = "2"
+		var customer models.Customer
+		if err := db.Where("id_name = ?", row[3]).First(&customer).Error; err != nil {
+			return nil, err
 		}
 
 		parsedTime, err := time.Parse(layout, row[1])
 		if err != nil {
-			panic("Error handling parse time")
+			return nil, err
 		}
 		melbourneTime := parsedTime.In(location)
 
 		Order := models.Order{
 			CreatedAt:  melbourneTime,
 			OrderName:  row[2],
-			CustomerID: customerIdData,
+			CustomerID: strconv.Itoa(int(customer.ID)),
 		}
 		Orders = append(Orders, Order)
 	}
-	return Orders
+	return Orders, nil
 }
